3b6bc59cf742c03e3063826f923f1d425a07a633
ThaProfessorMEX 🌮 <mora.lin36@gmail.com>
2021-08-02 14:02:13 -0700

    almost done

--- code.js
+++ code.js
@@ -1,23 +1,13 @@
-//Got starter code from demo recording
+//Got starter code from thursday/friday demo recording
 
 
 fallbackLocation ={latitude:-37.871972, longitude:175.689232};  // Hobbiton
-var photosArray =[];
+let photosArray =[];
 let currentPhoto = 0;
+let container = document.getElementById("photoContainer")
 
+let x = 0; 
 
-// function apiUrl(coords, term){
-//     "https://flickr.com/services/rest/" +
-//     "?api_key=200aed0990a7bf83e651c306dd7546e8" +
-//     "&format=json" +
-//     "&nojsoncallback=1" +
-//     "&method=flickr.photos.search" +
-//     "&safe_search=1" +
-//     "&per_page=5" +
-//     "&lat=" + coords.latitude +
-//     "&lon=" + coords.longitude +
-//     "&text=" + term; 
-// }
 
 function assembleImageSourceURL (photoObj) {
     return "https://farm" + photoObj.farm +
@@ -27,6 +17,22 @@ function assembleImageSourceURL (photoObj) {
 // console.log(assembleImageSourceURL(dogs))
 // const imageUrl = assembleImageSourceURL(response.photos.photo[0]);
 
+let button = document.getElementById("button");
+
+button.addEventListener("click", nextPhoto)
+let photoObj;
+function nextPhoto (){
+
+	if(currentPhoto < photosArray.length){
+		photoObj = photosArray[currentPhoto]
+		assembleImageSourceURL(photoObj)
+		console.log(assembleImageSourceURL(photoObj))
+		currentPhoto +=1
+	}
+console.log(photoObj)	
+}
+console.log(currentPhoto)
+console.log(photoObj)
 
 function showPhotos (data) {
 	console.log(data)
@@ -37,33 +43,29 @@ function showPhotos (data) {
 	// Look at the first photo and turn it into an <img src=___> tag
 	console.log(assembleImageSourceURL(photosArray[currentPhoto]))
     let img = document.createElement("img")
+	console.log(photoObj)
     img.src = assembleImageSourceURL(photosArray[currentPhoto])
+
 	// Append the image tag to the page
-    document.body.append(img)
-
-
+    container.append(img)
+	
 }
-
-
-button = document.getElementsByClassName("button");
-for(let i=0; i < photosArray.length; i ++){
-button[i].addEventListener("click", function (){
-    currentPhoto +=1
-});
-}
-
 function processResponse (response) {
 	let responsePromise = response.json()
 	responsePromise.then(showPhotos)
 }
-console.log(photosArray)
 
 function requestPhotos (location) {
 	console.log("Requesting photos near " + location.latitude + ", " + location.longitude)
 
 	let myApiKey = "200aed0990a7bf83e651c306dd7546e8"
-	let url = 'https://shrouded-mountain-15003.herokuapp.com/https://api.flickr.com/services/rest/?api_key=' + myApiKey + '&format=json&nojsoncallback=1&method=flickr.photos.search&safe_search=1&per_page=5&lat='+location.latitude +'&lon='+ location.longitude +'&text=car'
-
+	let url = "https://shrouded-mountain-15003.herokuapp.com/https://api.flickr.com/services/rest/?api_key="+ 
+	 myApiKey + 
+	 "&format=json&nojsoncallback=1&method=flickr.photos.search&safe_search=1&per_page=5&lat="+
+	 location.latitude +
+	 "&lon="+ 
+	 location.longitude 
+	
 	let fetchPromise = fetch(url)
 	fetchPromise.then(processResponse)
 }
@@ -85,5 +87,4 @@ let options = {
 }
 
 navigator.geolocation.getCurrentPosition(useCurrentLocation, useFallbackLocation, options)
-
 
